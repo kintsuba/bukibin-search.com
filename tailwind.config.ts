c13216632e06c089d5a1d7a96c5d7d718792c623
Kintsuba <bnxth107@gmail.com>
2023-10-15 03:57:41 +0900

    feat: tailwind's fonts setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,35 @@
 import type { Config } from "tailwindcss";
 
-export default <Partial<Config>>{};
+export default <Partial<Config>>{
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Segoe UI",
+          "Meiryo",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "sans-serif",
+        ],
+        display: [
+          "Segoe UI",
+          "Meiryo",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "sans-serif",
+        ],
+        body: [
+          "Segoe UI",
+          "Meiryo",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+};
